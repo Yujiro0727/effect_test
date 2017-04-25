918d6283cd11ddd5f16d5891a55b623fe95b18a9
kkkkoyo_ <mmkrz.sg@gmail.com>
2017-04-25 17:54:41 +0900

    add soundTest

---
+++ Assets/Scripts/SoundTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	public void SoundCoin()
+	{
+		SoundManager.Instance.PlaySe("coin");
+	}
+	public void SoundError()
+	{
+		SoundManager.Instance.PlaySe("error");
+	}
+}
